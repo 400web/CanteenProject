ad1f55cab3c779312198cb27efa580ec0a38ccdd
杨秀卓 <114325425+usstyxz@users.noreply.github.com>
2023-12-11 23:02:01 +0800

    删除mybatis-plus框架

---
+++ src/main/java/com/cp/service/impl/SystemAdminServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cp.service.impl;
+
+import com.cp.domain.SystemAdmin;
+import com.cp.mapper.SystemAdminMapper;
+import com.cp.service.SystemAdminService;
+
+public class SystemAdminServiceImpl implements SystemAdminService {
+    @Override
+    public SystemAdmin getSystemAdminById(String adminId) {
+        return null;
+    }
+
+    @Override
+    public boolean addSystemAdmin(SystemAdmin admin) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteSystemAdmin(String adminId) {
+        return false;
+    }
+}
