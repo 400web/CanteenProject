085bfffe862bd4b1b269e4f128d8fb51f2a050d5
杨秀卓 <114325425+usstyxz@users.noreply.github.com>
2023-12-13 12:14:50 +0800

    修改食堂食堂属性名

--- src/main/java/com/cp/domain/Canteen.java
+++ src/main/java/com/cp/domain/Canteen.java
@@ -11,11 +11,11 @@ import lombok.*;
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class Canteen {
-    private String canteenId;// 食堂ID
-    private String canteenName;// 食堂名称
-    private String canteenLocation;// 食堂位置
-    private String canteenIntroduction;// 食堂简介
+    private String id;// 食堂ID
+    private String name;// 食堂名称
+    private String location;// 食堂位置
+    private String introduction;// 食堂简介
     private String openingTime;// 食堂开门时间
     private String closingTime;// 食堂关门时间
-    private int canteenRating;// 食堂等级分
+    private int rating;// 食堂等级分
 }
