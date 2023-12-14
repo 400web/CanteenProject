c6e6bd7c120849c5c8fbcfe126ed2168c08e7668
Seul-w <144869334+Seul-w@users.noreply.github.com>
2023-12-14 23:41:16 +0800

    食堂界面

--- src/main/java/com/cp/domain/Canteen.java
+++ src/main/java/com/cp/domain/Canteen.java
@@ -15,7 +15,15 @@ public class Canteen {
     private String name;// 食堂名称
     private String location;// 食堂位置
     private String introduction;// 食堂简介
-    private String openingTime;// 食堂开门时间
-    private String closingTime;// 食堂关门时间
+    private String businessTime;// 食堂营业时间
+    //private String closingTime;// 食堂关门时间
+    private String activity;//食堂活动公告
     private int rating;// 食堂等级分
+    public Canteen(String name,String introduction,String businessTime,String activity){
+        this.activity=activity;
+        this.businessTime=businessTime;
+        this.name=name;
+        this.introduction=introduction;
+    }
 }
+
