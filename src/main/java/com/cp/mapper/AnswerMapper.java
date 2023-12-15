2a9063b1b095ad1d376c7a9ab77c0d615290fdf0
杨秀卓 <114325425+usstyxz@users.noreply.github.com>
2023-12-15 10:39:43 +0800

    mapper全部实现

--- src/main/java/com/cp/mapper/AnswerMapper.java
+++ src/main/java/com/cp/mapper/AnswerMapper.java
@@ -1,5 +1,14 @@
 package com.cp.mapper;
 
 
+import com.cp.domain.Answer;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface AnswerMapper {
+    Answer selectById(@Param("userId") String userId, @Param("optionId") String optionId);
+
+    List<Answer> selectList();
+    boolean addAnswer(Answer answer);
 }
