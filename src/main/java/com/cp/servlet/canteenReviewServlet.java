7f0ca458fe27186a1e8b58d1defda6ae2e77cf9d
Seul-w <144869334+Seul-w@users.noreply.github.com>
2023-12-21 16:32:39 +0800

    食堂中菜品评价和食堂评价修改

---
+++ src/main/java/com/cp/servlet/canteenReviewServlet.java
@@ -0,0 +1,28 @@
+package com.cp.servlet;
+
+import com.cp.domain.CanteenReview;
+import com.cp.service.CanteenReviewService;
+import com.cp.service.impl.CanteenReviewServiceImpl;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "canteenReviewServlet", value = "/canteenReviewServlet")
+public class canteenReviewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String canteenId=request.getParameter("canteenId");
+            CanteenReviewService canteenReviewService=new CanteenReviewServiceImpl();
+            List<CanteenReview> canteenReviewList=canteenReviewService.getReviewsByCanteenId(canteenId);
+            request.setAttribute("canteenReviewList",canteenReviewList);
+            request.getRequestDispatcher("canteenReview.jsp").forward(request,response);
+    }
+}
