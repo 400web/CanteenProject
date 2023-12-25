830f1136ada24d1b4a504545607ae015a45bd3c1
魏京阳 <dfydn92@gmail.com>
2023-12-26 00:50:06 +0800

    食堂管理员界面的编辑菜单和修改食堂信息

---
+++ src/main/java/com/cp/servlet/changeCanteenServlet.java
@@ -0,0 +1,38 @@
+package com.cp.servlet;
+
+import com.cp.domain.Canteen;
+import com.cp.service.CanteenService;
+import com.cp.service.impl.CanteenServiceImpl;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "changeCanteenServlet", value = "/changeCanteenServlet")
+public class changeCanteenServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从请求中获取食堂信息
+        String canteenName = request.getParameter("name");
+        String canteenDescription = request.getParameter("description");
+        String openingTime = request.getParameter("openingTime");
+        String closingTime = request.getParameter("closingTime");
+
+        // 创建食堂对象并设置属性
+        Canteen canteen = new Canteen();
+        canteen.setName(canteenName);
+        canteen.setIntroduction(canteenDescription);
+        canteen.setOpeningTime(openingTime);
+        canteen.setClosingTime(closingTime);
+
+        // 使用服务层更新食堂信息
+        CanteenService canteenService = new CanteenServiceImpl();
+        canteenService.updateCanteen(canteen);
+    }
+}
