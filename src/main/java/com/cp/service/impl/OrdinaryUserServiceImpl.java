76f4064a0ac48d872b48a6b28bb74c7c36c5c7e6
杨秀卓 <114325425+usstyxz@users.noreply.github.com>
2023-12-25 22:31:37 +0800

    普通用户业务更新

--- src/main/java/com/cp/service/impl/OrdinaryUserServiceImpl.java
+++ src/main/java/com/cp/service/impl/OrdinaryUserServiceImpl.java
@@ -2,21 +2,30 @@ package com.cp.service.impl;
 
 
 import com.cp.domain.OrdinaryUser;
+import com.cp.mapper.OrdinaryUserMapper;
 import com.cp.service.OrdinaryUserService;
+import com.cp.utils.MybatisUtils;
 
 public class OrdinaryUserServiceImpl implements OrdinaryUserService {
+    OrdinaryUserMapper ordinaryUserMapper = MybatisUtils.getMapper(OrdinaryUserMapper.class);
+
     @Override
     public OrdinaryUser getOrdinaryUserById(String userId) {
-        return null;
+        return ordinaryUserMapper.selectById(userId);
     }
 
     @Override
     public boolean addOrdinaryUser(OrdinaryUser user) {
-        return false;
+        return ordinaryUserMapper.addOrdinaryUser(user);
     }
 
     @Override
     public boolean deleteOrdinaryUser(String userId) {
-        return false;
+        return ordinaryUserMapper.deleteOrdinaryUser(userId);
+    }
+
+    @Override
+    public boolean updateOrdinaryUser(OrdinaryUser user) {
+        return ordinaryUserMapper.updateOrdinaryUser(user);
     }
 }
